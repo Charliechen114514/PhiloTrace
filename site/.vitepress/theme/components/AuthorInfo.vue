6d51e2638a2d1a72fe8cf0fe357b5272fc15aa1a
Charliechen114514 <725610365@qq.com>
2026-06-13 14:09:08 +0800

    feat: show author

---
+++ site/.vitepress/theme/components/AuthorInfo.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useData } from 'vitepress'
+
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <div v-if="frontmatter.author" class="author-block">
+    <div class="author-line">
+      <span class="author-label">作者</span>
+      <span class="author-name">{{ frontmatter.author }}</span>
+      <span v-if="frontmatter.date" class="author-sep">·</span>
+      <span v-if="frontmatter.date" class="author-date">{{ frontmatter.date }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.author-block {
+  margin-top: 2rem;
+  padding-top: 1.5rem;
+  border-top: 1px solid var(--vp-c-divider);
+}
+.author-line {
+  color: var(--vp-c-text-2);
+  font-size: 0.875rem;
+  line-height: 1.6;
+}
+.author-label {
+  font-weight: 600;
+  margin-right: 0.25rem;
+}
+.author-name {
+  color: var(--vp-c-text-1);
+}
+.author-sep {
+  margin: 0 0.5rem;
+}
+</style>
--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -5,6 +5,7 @@ import DialogueTurn from './components/DialogueTurn.vue'
 import OrganizerNote from './components/OrganizerNote.vue'
 import ConceptCard from './components/ConceptCard.vue'
 import RecentPosts from './components/RecentPosts.vue'
+import AuthorInfo from './components/AuthorInfo.vue'
 import { setupMermaid } from './mermaid-client'
 import './custom.css'
 
@@ -14,6 +15,8 @@ export default {
     return h(DefaultTheme.Layout, null, {
       // 首页 features 之后注入"最近更新"列表（内容流演示）
       'home-features-after': () => h(RecentPosts),
+      // 文章末尾自动显示作者署名（读取 frontmatter.author）
+      'doc-footer-before': () => h(AuthorInfo),
     })
   },
   setup() {
